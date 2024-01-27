b7f419f88428e44d155a5842c268ef21df4ea0d9
rafael747cardoso <rafael350cardoso@protonmail.com>
2024-01-27 02:03:26 -0300

    new pic

--- script.R
+++ script.R
@@ -60,7 +60,7 @@ dt_districts <- dt_districts[dt_districts$name_muni == "São Paulo", ]
 dt_subdistrict <- sf::st_read("git_repo/WalkSafeSP/shapefiles/neighborhoods.shp")
 st_crs(dt_subdistrict) <- st_crs(31983)
 dt_subdistrict <- st_transform(dt_subdistrict, crs = 4326)
-dt_subdistrict <- dt_subdistrict[dt_subdistrict$od_municip == "36",]
+dt_subdistrict <- dt_subdistrict[dt_subdistrict$od_municip == "36", ]
 
 dt_poly <- dt_subdistrict
 
@@ -88,19 +88,51 @@ dt_poly <- readRDS("dt_poly.RDS")
 dt_poly$od_area <- as.numeric(dt_poly$od_area)
 dt_poly$occurrences_per_area <- dt_poly$occurrences/dt_poly$od_area
 
+# New variables
 dt_poly$rob_norm <- dt_poly$occurrences_per_area/max(dt_poly$occurrences_per_area)
 dt_poly$rob_norm[dt_poly$rob_norm == 0] <- min(dt_poly$rob_norm[dt_poly$rob_norm > 0])
-dt_poly$rob_log <- log(dt_poly$rob_norm)
+dt_poly$rob_log <- -log(dt_poly$rob_norm)
+dt_poly$rob_log <- -dt_poly$rob_log
+
+
+
+
+# Add layers
+
+## Subway lines
+dt_subway_lines <- sf::st_read("git_repo/WalkSafeSP/shapefiles/subway_lines.shp")
+st_crs(dt_subway_lines) <- st_crs(31983)
+dt_subway_lines <- st_transform(dt_subway_lines, crs = 4326)
+dt_subway_lines$color <- c("#002CEF", "#00EF17", "#EF0000", "#EF00E0", "#C2C2C2", "#FEFA00")
+
+## Subway stations
+dt_subway_stations <- sf::st_read("git_repo/WalkSafeSP/shapefiles/subway_stations.shp")
+st_crs(dt_subway_stations) <- st_crs(31983)
+dt_subway_stations <- st_transform(dt_subway_stations, crs = 4326)
+station_colors <- data.frame(
+  color = c("#002CEF", "#00EF17", "#EF0000", "#EF00E0", "#C2C2C2", "#FEFA00"),
+  color_name = c("AZUL", "VERDE", "VERMELHA", "LILAS", "PRATA", "AMARELA")
+)
+dt_subway_stations <- merge(dt_subway_stations, station_colors, 
+                            by.x = "emt_linha",
+                            by.y = "color_name")
+
+## Slums
+dt_slums <- sf::st_read("git_repo/WalkSafeSP/shapefiles/slums.shp")
+st_crs(dt_slums) <- st_crs(31983)
+dt_slums <- st_transform(dt_slums, crs = 4326)
 
 
 
 # Create a leaflet map
+
 x_name = "rob_log"
 x_val <- dt_poly[[x_name]]
 bins <- seq(from = min(x_val),
             to = max(x_val),
             length.out = 9)
-pal <- colorBin(palette = "YlOrRd",
+palette_name = "BuPu"
+pal <- colorBin(palette = palette_name,
                 domain = x_val, 
                 bins = bins)
 labels <- sprintf(
@@ -142,12 +174,51 @@ leaflet(dt_poly) %>%
       direction = "auto"
     )
   ) %>%
+  addPolylines(
+    data = dt_subway_lines,
+    color = ~color,
+    weight = 4
+  ) %>%
+  addCircleMarkers(
+    data = dt_subway_stations,
+    fillColor = ~color,
+    radius = 3,
+    fillOpacity = 0.8,
+    weight = 1
+  ) %>%
+  addPolygons(
+    data = dt_slums,
+    fillColor = "#FF9D00",
+    weight = 0.5,
+    opacity = 1,
+    color = "#FF9D00",
+    fillOpacity = 0.7,
+    labelOptions = labelOptions(
+      style = list(
+        "font-weight" = "normal",
+        padding = "3px 8px"
+      ),
+      textsize = "15px",
+      direction = "auto"
+    )
+  ) %>%
   addLegend(
     "bottomright",
-    pal = colorNumeric("YlOrRd", domain = x_val),
+    pal = colorNumeric(palette_name, domain = x_val),
     values = x_val,
     title = x_name,
-    opacity = 1
+    opacity = 1,
+    layerId = "legend"
+  ) %>%
+  addControl(
+    html = "
+    <div id='legend' 
+      style='background-color: #FF9D00; 
+      padding: 5px;
+      border-radius: 5px;
+      color: white;
+    '>Slums</div>",
+    position = "bottomright"
   )
 
 
