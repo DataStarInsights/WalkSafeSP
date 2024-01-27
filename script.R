decdb0321368b8e93e327c1afa1de0404b60e0cb
rafael747cardoso <rafael350cardoso@protonmail.com>
2024-01-27 00:59:47 -0300

    new scale and grid

--- script.R
+++ script.R
@@ -30,10 +30,10 @@ library(data.table)
 library(leaflet)
 library(leaflet.extras)
 library(DT)
+library(sf)
 
 
 # Mobile phone robbery occurences 
-
 dt <- readRDS("dt.RDS")
 
 dt$DATA_OCORRENCIA_BO <- as.Date(dt$DATA_OCORRENCIA_BO)
@@ -52,19 +52,22 @@ dt <- dt[dt$LATITUDE < 0 & dt$LONGITUDE < 0, ]
 
 plot(y = dt$LATITUDE, x = dt$LONGITUDE)
 
-
 # District polygons
+dt_districts <- geobr::read_neighborhood()
+dt_districts <- dt_districts[dt_districts$name_muni == "São Paulo", ]
 
-dt_poly <- geobr::read_neighborhood()
-dt_poly <- dt_poly[dt_poly$name_muni == "São Paulo", ]
+# Subdistrict polygons
+dt_subdistrict <- sf::st_read("git_repo/WalkSafeSP/shapefiles/neighborhoods.shp")
+st_crs(dt_subdistrict) <- st_crs(31983)
+dt_subdistrict <- st_transform(dt_subdistrict, crs = 4326)
+dt_subdistrict <- dt_subdistrict[dt_subdistrict$od_municip == "36",]
 
+dt_poly <- dt_subdistrict
 
-# Occurences per district
-
+# Occurences per subdistrict
 occurrences <- c()
 for (i in 1:nrow(dt_poly)) {
-  poly_coor <- data.table::data.table(dt_poly$geom[i][[1]][[1]][[1]])
-  
+  poly_coor <- data.table::data.table(dt_poly$geom[i][[1]][[1]])
   n_i <- 0
   for (j in 1:nrow(dt)) {
     n_i <- n_i + sp::point.in.polygon(point.x = dt$LONGITUDE[j],
@@ -72,33 +75,37 @@ for (i in 1:nrow(dt_poly)) {
                                       pol.x = poly_coor$V1,
                                       pol.y = poly_coor$V2)
   }
-  
   occurrences <- c(occurrences, n_i)
-  
   print(paste0(100*i/nrow(dt_poly), " %"))
 }
-
 dt_poly$occurrences <- occurrences
 
-# Convert the CRS to WGS84
-
-dt_poly <- sf::st_transform(dt_poly, 4326)  # 4326 is the EPSG code for WGS84
-
 saveRDS(dt_poly, "dt_poly.RDS")
-
-
 dt_poly <- readRDS("dt_poly.RDS")
 
 
-# Create a leaflet map
+# Occurences over poylgon area
+dt_poly$od_area <- as.numeric(dt_poly$od_area)
+dt_poly$occurrences_per_area <- dt_poly$occurrences/dt_poly$od_area
 
-bins <- seq(from = min(dt_poly$occurrences),
-            to = max(dt_poly$occurrences) + 101,
-            by = 100)
-pal <- colorBin("YlOrRd", domain = dt_poly$occurrences, bins = bins)
+dt_poly$rob_norm <- dt_poly$occurrences_per_area/max(dt_poly$occurrences_per_area)
+dt_poly$rob_norm[dt_poly$rob_norm == 0] <- min(dt_poly$rob_norm[dt_poly$rob_norm > 0])
+dt_poly$rob_log <- log(dt_poly$rob_norm)
+
+
+
+# Create a leaflet map
+x_name = "rob_log"
+x_val <- dt_poly[[x_name]]
+bins <- seq(from = min(x_val),
+            to = max(x_val),
+            length.out = 9)
+pal <- colorBin(palette = "YlOrRd",
+                domain = x_val, 
+                bins = bins)
 labels <- sprintf(
-  "<strong>%s</strong><br/>%g assaltos</sup>",
-  dt_poly$name_district, dt_poly$occurrences
+  paste0("<strong>%s</strong><br/>%g</sup>"),
+  dt_poly$od_nome, x_val
 ) %>% lapply(htmltools::HTML)
 
 
@@ -106,19 +113,23 @@ leaflet(dt_poly) %>%
   addTiles(
     options = providerTileOptions(opacity = 0.8)
   ) %>%
-  setView(lat = -23.550520, lng = -46.6333085, zoom = 11) %>%
+  setView(
+    lat = -23.550520, 
+    lng = -46.6333085, 
+    zoom = 11
+  ) %>%
   addPolygons(
-    fillColor = ~pal(occurrences),
-    weight = 1,
+    fillColor = ~pal(get(x_name)),
+    weight = 0.5,
     opacity = 1,
     color = "white",
     dashArray = "3",
-    fillOpacity = 0.6,
+    fillOpacity = 0.7,
     highlightOptions = highlightOptions(
       weight = 5,
       color = "#666",
       dashArray = "",
-      fillOpacity = 0.7,
+      fillOpacity = 0.8,
       bringToFront = TRUE
     ),
     label = labels,
@@ -133,9 +144,9 @@ leaflet(dt_poly) %>%
   ) %>%
   addLegend(
     "bottomright",
-    pal = colorNumeric("YlOrRd", domain = dt_poly$occurrences),
-    values = dt_poly$occurrences,
-    title = "Occurrences",
+    pal = colorNumeric("YlOrRd", domain = x_val),
+    values = x_val,
+    title = x_name,
     opacity = 1
   )
 
